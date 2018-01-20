3c96aad2862095376f065b06e698edf54edaee7c
Lucien Le Roux <lucien.le-roux@epitech.eu>
2018-01-20 17:49:13 +0100

    Updated package.json to use appropriate blih version

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,14 +18,10 @@ module.exports = {
     'html'
   ],
   'rules': {
-    // allow paren-less arrow functions
     'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'no-console': 0,
-    'no-unused-vars': 0,
-    'no-undef': 0
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-unused-vars': [2, {
+        'argsIgnorePattern': '^_'
+    }]
   }
 }
